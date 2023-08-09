454a37d2f5c62787c8011eefa1af5d69f1785a5f
Gameunkulus <bugr24-8@mail.ru>
2023-08-09 19:55:35 +0300

    program redacted 6

--- src/test/java/ru/netology/CallBackTest.java
+++ src/test/java/ru/netology/CallBackTest.java
@@ -1,7 +1,10 @@
 package ru.netology;
 
+import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.SelenideElement;
 import org.junit.jupiter.api.Test;
+
+import java.time.Duration;
 
 import static com.codeborne.selenide.Condition.exactText;
 import static com.codeborne.selenide.Selenide.$;
@@ -11,14 +14,15 @@ public class CallBackTest {
     @Test
     void shouldTest() {
         open("http://localhost:9999");
-        SelenideElement form = $("[class=form form_size_m form_theme_alfa-on-white]");
-        form.$("[data-test-id=city] input").sendKeys(" Ставрополь");
-        form.$("[data-test-id=date] input").setValue("10.05.2025");
-        form.$("[data-test-id=name] input").setValue("Петр Петрович");
-        form.$("[data-test-id=phone] input").setValue("+79270000000");
-        form.$("[data-test-id=agreement]").click();
-        form.$("button.button").click();
-        $("[data-test-id=notification]").shouldHave(exactText("Встреча успешно забронирована на " + "10.05.2025"));
+        $("[data-test-id=city] input").setValue("Ставрополь");
+        $("[data-test-id=date] input").sendKeys("10.05.2025");
+        $("[data-test-id=name] input").setValue("Петр Петрович");
+        $("[data-test-id=phone] input").setValue("+79270000000");
+        $("[data-test-id=agreement]").click();
+        $("button.button").click();
+        $("[data-test-id=notification]")
+                .shouldBe(Condition.visible, Duration.ofSeconds(15))
+                .shouldHave(Condition.exactText("Встреча успешно забронирована на" + "10.05.2025"));
     }
 
 }
