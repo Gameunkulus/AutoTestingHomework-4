3d23e80d1654ef618a36d44d7beb90319e314caf
Gameunkulus <bugr24-8@mail.ru>
2023-08-09 20:20:16 +0300

    program redacted 7

--- src/test/java/ru/netology/CallBackTest.java
+++ src/test/java/ru/netology/CallBackTest.java
@@ -5,6 +5,8 @@ import com.codeborne.selenide.SelenideElement;
 import org.junit.jupiter.api.Test;
 
 import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 import static com.codeborne.selenide.Condition.exactText;
 import static com.codeborne.selenide.Selenide.$;
@@ -15,6 +17,7 @@ public class CallBackTest {
     void shouldTest() {
         open("http://localhost:9999");
         $("[data-test-id=city] input").setValue("Ставрополь");
+        String curDate = generateDate
         $("[data-test-id=date] input").sendKeys("10.05.2025");
         $("[data-test-id=name] input").setValue("Петр Петрович");
         $("[data-test-id=phone] input").setValue("+79270000000");
